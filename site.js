d236734150598b80182d8bee4a786a08ae6bdf98
Andy Yang <500158274@fvtc.edu>
2024-01-26 20:40:27 -0600

    worked on site.js

--- site.js
+++ site.js
@@ -1,16 +1,10 @@
+const link = document.querySelectorAll('.copyright')
 
-const h1 = document.querySelector('h1')
-h1.textContent = "Ryan's lame webpage!"
-
-const copyright = document.querySelector('footer > p')
-copyright.textContent = "BLA"
-
-const link = document.querySelector('.active')
-link.style.color = "#ff0000"
-link.style.textDecoration = "underline"
-
-const contactLink = document.querySelector('#contact')
-contactLink.style.fontWeight = "bold"
-
-const links = document.querySelectorAll('nav a')
-links.forEach(link => link.style.display = 'none')
+setTimeout(() => {
+    link.forEach(link => {
+        link.style.color = 'red'
+    })
+}, 3000)
+link.forEach(link => {
+    link.style.fontWeight = 'bold'
+})
